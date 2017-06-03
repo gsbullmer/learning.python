86a05c3f8b932ff79e49310bfc9bd212dbb66432
Greg Bullmer <gsbullmer@gmail.com>
2017-06-03 17:15:39 -0500

    feat: FInish chapter 1

---
+++ ch1/bike.py
@@ -0,0 +1,21 @@
+# let's define the class Bike
+class Bike:
+    def __init__(self, color, frame_material):
+        self.color = color
+        self.frame_material = frame_material
+
+    def brake(self):
+        print("Braking!")
+
+# let's create a couple instances
+red_bike = Bike('Red', 'Carbon fiber')
+blue_bike = Bike('Blue', 'Steel')
+
+# let's inspect the objects we have, instances of the Bike class.
+print(red_bike.color) # prints: Red
+print(red_bike.frame_material) # prints: Carbon fiber
+print(blue_bike.color) # prints: Blue
+print(blue_bike.frame_material) # prints: Steel
+
+# let's brake!
+red_bike.brake() # prints: Braking!
